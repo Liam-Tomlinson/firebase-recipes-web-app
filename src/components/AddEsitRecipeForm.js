528005961fae96f891388510a2d732bc09f84647
Liam Tomlinson <liamtomlinson@Liams-MacBook-Air.local>
2024-03-07 23:36:26 +0000

    bug fix

--- src/components/AddEsitRecipeForm.js
+++ src/components/AddEsitRecipeForm.js
@@ -1,17 +1,13 @@
-import { useState } from "react";
+//import { useState } from "react";
 
-function AddEditRecipeForm( handleAddRecipe) {
+function AddEditRecipeForm(handleAddRecipe) {
+  // const [name, setName] = useState("");
+  // const [category, setCategory] = useState("");
+  // const [publishDate, setPublishDate] = useState(new Date().toISOSstring().split('T')[0]);
 
-    // const [name, setName] = useState("");
-    // const [category, setCategory] = useState("");
-    // const [publishDate, setPublishDate] = useState(new Date().toISOSstring().split('T')[0]);
-    
-    // const [directions, setDirections] = useState("");
-    // const [ingredients, setIngredients] = useState([]);
-    // const [ingredientName, setIngredientName] = useState("");
-
-
-
+  // const [directions, setDirections] = useState("");
+  // const [ingredients, setIngredients] = useState([]);
+  // const [ingredientName, setIngredientName] = useState("");
 
   return <h1>Add edit recipe form</h1>;
 }
